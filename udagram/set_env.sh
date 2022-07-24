32e11b75e9799a5dd6373f7bf5c14a4848816ea0
Terry Cheng <terryhycheng@gmail.com>
2022-07-24 16:11:11 +0800

    edited

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=69609055
+export POSTGRES_HOST=db-terryhycheng-deployment.chmxdxlnoaap.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=terryhycheng-testing-123455
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
