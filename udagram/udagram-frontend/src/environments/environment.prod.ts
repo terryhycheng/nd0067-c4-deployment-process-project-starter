813d00796493e3af3b5720422243a40a9934d078
Terry Cheng <terryhycheng@gmail.com>
2022-07-24 18:05:58 +0800

    eb setting updated

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0',
+  apiHost: 'https://udagram-api-dev2222222.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
