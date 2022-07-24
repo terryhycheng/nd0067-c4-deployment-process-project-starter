521fa399dcad8c50357e5ea54ba0c0593216bbfb
Terry Cheng <terryhycheng@gmail.com>
2022-07-24 23:11:02 +0800

    another try

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=69609055
-export POSTGRES_HOST=db-terryhycheng-deployment.chmxdxlnoaap.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=db-terryhycheng-aws.cav35efz4mbi.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=terryhycheng-testing-123455
+export AWS_BUCKET=terryhycheng-aws-deployment
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
