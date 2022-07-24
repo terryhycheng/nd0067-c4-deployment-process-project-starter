50a64bb57e8edcd6508ebc163f59f99eafd76fe5
Terry Cheng <terryhycheng@gmail.com>
2022-07-24 18:37:19 +0800

    API links changed

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'https://udagram-api-dev2222222.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-api-dev2222222.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
